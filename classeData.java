a1d2a827a836cdc6a338282c6c9d1590a85b5d54
Beatriz Novaes <144396445+beatriznovaesr@users.noreply.github.com>
2024-03-20 17:46:22 -0300

    Update Data.java

--- classeData.java
+++ classeData.java
@@ -1,11 +1,11 @@
-package classeCNH;
+package transportadora;
 
-public class classeData {
+public class Data {
 	private int dia;
 	private int mes;
 	private int ano;
 
-	public classeData(int dia, int mes, int ano) {
+	public Data(int dia, int mes, int ano) {
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;	
