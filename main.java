e279cce3449a6703b6d30ab79ad2c6ac5c00b20f
Julia Odin <147524921+juliaa-odin@users.noreply.github.com>
2024-03-22 08:57:01 -0300

    Add files via upload

--- main.java
+++ main.java
@@ -22,7 +22,6 @@ public class main {
 
 		System.out.println("Informações do Endereço após a mudança do número:");
         System.out.println(endereco1.toString());
-		
 		//endereco1.excluirEndereco();
 		//endereco1.toString();
 
@@ -42,6 +41,30 @@ public class main {
         System.out.println(motorista1.toString());
 		//motorista1.excluirMotorista();
 		//motorista1.toString();
+
+		Veiculo veiculo1 = new Veiculo("Uno", "22HT67A", "Fiat", 2010, tipoVeiculo.CARRO);
+
+		Transportadora transportadora1 = new Transportadora("Correio", "34.555.022/0001-10");
+
+		System.out.println("Informações do Veículo:");
+        System.out.println(veiculo1.toString());
+
+		veiculo1.editarVeiculo("Mobi", "56KE01D", "Fiat", 2018, tipoVeiculo.CARRO);
+
+        System.out.println("Novas informações do Veículo:");
+        System.out.println(veiculo1.toString());
+		//veiculo1.excluirVeiculo();
+		//veiculo1.toString();
+
+		System.out.println("Informações da Transportadora:");
+		System.out.println(transportadora1.toString());
+
+		transportadora1.editarTransportadora("Express Transporte", "88.332.430/0002-09");
+
+		System.out.println("Novas informações da Transportadora:");
+		System.out.println(transportadora1.toString());
+		//transportadora1.excluirTransportadora();
+		//transportadora1.toString();
 	}
 
 }
