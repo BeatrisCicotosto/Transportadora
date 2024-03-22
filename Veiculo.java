9c4d8b8a617384781df041216a94f2f6a0a11e2a
Julia Odin <147524921+juliaa-odin@users.noreply.github.com>
2024-03-21 23:16:52 -0300

    Add files via upload

--- Veiculo.java
+++ Veiculo.java
@@ -7,14 +7,56 @@ public class Veiculo {
 	String marca;
 	int ano;
 
+	
 	public Veiculo(String modelo, String placa, String marca,
 	int ano, String tipoVeiculo) {
 		this.modelo = modelo;
 		this.placa = placa;
 		this.marca = marca;
 		this.ano = ano;
-		this.tipoVeiculo = tipoveiculo;
+		this.tipoVeiculo = tipoVeiculo;
 	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getTipoVeiculo() {
+		return tipoVeiculo;
+	}
+
+	public void setTipoVeiculo(String tipoVeiculo) {
+		this.tipoVeiculo = tipoVeiculo;
+	}
+
 
 	public String toString() {
 		return "Placa: " + this.placa +
