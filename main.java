660600c7f19545347e276e3cb0b715d15c50cac3
anasrnogueira <137672653+anasrnogueira@users.noreply.github.com>
2024-03-22 20:19:33 -0300

    main.java

--- main.java
+++ main.java
@@ -4,9 +4,21 @@ public class main {
 		CNH cnh1 = new CNH("125476467", Categoria.A, validadeCNH);
 		cnh1.exibirCNH();
 
+        Cliente  cliente1  = new Cliente ("Jessica", "12788663776", "998771356,", "Rua XV, 12, Centro, Maringa, PR", "girafa12@gmail.com", "Geladeira");
+
 		Endereco endereco1 = new Endereco("Rua Piu XII", 12, "Centro", "Londrina", "PR", 86010070);
 
 		Motorista motorista1 = new Motorista("João", "123456789", endereco1, cnh1);
+
+        System.out.println("Informações do Cliente:");
+        System.out.println(cliente1.toString());
+
+        cliente1.editarCliente("Josias", "12788663776", "998771356,", "Rua XVX, 21, Centro, Maringa, PR", "girafa12@gmail.com", "Jogo de facas");
+        
+        System.out.printl("Novas informações do Cliente");
+        System.out.printl(cliente1.toString());
+        //cliente1.excluirCliente();
+        //cliente1.toString();
 
 		System.out.println("Informações do Endereço:");
 		System.out.println(endereco1.toString());
@@ -44,6 +56,8 @@ public class main {
 
 		Veiculo veiculo1 = new Veiculo("Uno", "22HT67A", "Fiat", 2010, tipoVeiculo.CARRO);
 
+        Produto produto1 = new produto("5543876", "R$350,00", 1, "Grande", "Jessica", "Rua XV, 12, Centro, Maringa, PR");
+
 		Transportadora transportadora1 = new Transportadora("Correio", "34.555.022/0001-10");
 
 		System.out.println("Informações do Veículo:");
@@ -56,6 +70,16 @@ public class main {
 		//veiculo1.excluirVeiculo();
 		//veiculo1.toString();
 
+        System.out.println("Informações do Produto");
+        System.out.println(produto1.toString());
+
+        produto1.editarProduto("5544876", "R$400,00", 1, "Pequeno", "Jessica", "Rua XV, 15, Centro, Maringa, PR");
+
+        System.out.println("Novas informações do Produto");
+        System.out.println(produto1.toString());
+        //produto1.excluirProduto();
+        //produto1.toString();
+        
 		System.out.println("Informações da Transportadora:");
 		System.out.println(transportadora1.toString());
 
